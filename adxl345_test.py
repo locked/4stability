dc5c4f0256cb300e26cd65346aea657adc08ddd5
root <root@kumiko.(none)>
2013-12-25 20:23:13 +0000

    tabify

--- adxl345_test.py
+++ adxl345_test.py
@@ -3,11 +3,11 @@ import sys
 import time
 
 if __name__ == "__main__":
-    accel = adxl345.ADXL345()
+	accel = adxl345.ADXL345()
     
-    while(True):
-        axis = accel.getAxes()
-        sys.stdout.write("\rx:%.3f y:%.3f z:%.3f" % (axis['x'], axis['y'], axis['z']))
-	sys.stdout.flush()
-        time.sleep(0.02)
+	while(True):
+		axis = accel.getAxes()
+		sys.stdout.write("\rx:%.3f y:%.3f z:%.3f" % (axis['x'], axis['y'], axis['z']))
+		sys.stdout.flush()
+		time.sleep(0.02)
 
