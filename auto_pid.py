e03aabbcb62a215beae505c7efeb17d710a968f2
LouisAdam <louis.adam@gmail.com>
2014-02-13 21:37:58 +0100

    corrections calculs angle. initialisation gyro

--- auto_pid.py
+++ auto_pid.py
@@ -21,13 +21,27 @@ parser.add_option("-p", "--p", dest="p", help="")
 enable_motor = True
 enable_mpu_dmp = False
 enable_curse = True
+RAD_TO_DEG = 57.3
+DEG_TO_RAD = 1 / RAD_TO_DEG
+ACCEL_SF = 0.004
 
 if enable_motor:
 	m = motor.Motor(debug=False)
 	m.init()
 
 accel = adxl345.ADXL345(bwrate=adxl345.ADXL345.BW_RATE_25HZ, range=adxl345.ADXL345.RANGE_2G)
-dist = hcsr04.HCSR04()
+dist  = hcsr04.HCSR04()
+mpu   = mpu6050.MPU6050()
+mpu.initialize()
+mpu.setRate(39) # 1khz / (1 + 4) = 200 Hz [9 = 100 Hz]
+#mpu.dmpInitialize()
+#mpu.setDMPEnabled(True)
+
+# Init gyro et angle
+data = mpu.readall()
+gyro_init = data['gyro_scaled']	
+axis = accel.getAxes()
+angle_rad = math.atan(axis['y']/axis['z']) if axis['z'] <> 0 else 0
 
 speed_percent = 0
 
@@ -44,33 +58,31 @@ try:
 	speed_percent = 10
 	diff_speed = 0
 	error = 0
-	dt = 0
+	dt_ms = 0
 	Kp = 0.001
-	Ki = 0.00000
+	Ki = 0
 	Kd = 0.002
 	KpDiff = 0.0001
 	KiDiff = 0.00001
 	KdDiff = 0.0001
-	target = 0
-	start_time = time.time()*100000
+	target_deg = 0
+	start_time = time.time()*1000000 # en µs
 	lines = []
 	speed_percent = 18
 	if enable_motor:
 		pos = m.set_speed(speed_percent/100.0)
 	time.sleep(3)
+	
 	ys = []
-	angle_rad = 0
+	#angle_rad = 0
 	axis = {}
-	mpu = mpu6050.MPU6050()
-	mpu.initialize()
-	mpu.setRate(39) # 1khz / (1 + 4) = 200 Hz [9 = 100 Hz]
-	#mpu.dmpInitialize()
-	#mpu.setDMPEnabled(True)
 	while (True):
+		init_time = time.time()
+		
 		distance = 0 #dist.measure()
 		axis = accel.getAxes()
 		data = mpu.readall()
-		gyro = data['gyro_scaled']
+		gyro = data['gyro_scaled'] - gyro_init
 		mpu_accel = data['accel_scaled']
 
 		#(axis['x'], axis['y'], axis['z'], fifocount) = mpu.getYPR()
@@ -89,29 +101,31 @@ try:
 		if c == 'e': Kd = Kd + KdDiff
 		if c == 'd': Kd = Kd - KdDiff if Kd - KdDiff >= 0 else 0
 
-		angle = 0
-		atan = 0
-		asin = 0
+		angle_deg = 0
+		atan_rad  = 0
+		asin_rad  = 0
 		p1 = 0
 		p2 = 0
 		if lastt > 0:
-			dt = (time.time()*1000 - lastt) / 1000
+			dt_ms = (time.time()*1000 - lastt) / 1000
 
 			# angle
-			p1 = 0.9
+			p1 = 0.98
 			p2 = 1 - p1
-			atan = math.atan(axis['y']/axis['z']) if axis['z'] <> 0 else 0
-			y = axis['y']*0.004
-			if y>1: y = 1
-			if y<-1: y = -1
-			asin = -math.asin(y)
-			angle_rad = p1 * (angle_rad - (gyro['y']/20) * dt) + p2 * atan
-			angle = angle_rad * 28.66
+			atan_rad = math.atan(axis['y']/axis['z']) if axis['z'] <> 0 else 0
+			# y = axis['y'] * ACCEL_SF
+			# if y>1: y = 1
+			# if y<-1: y = -1
+			# asin_rad = -math.asin(y)
+			asin_rad  = 0
+			angle_rad = p1 * (angle_rad - (gyro['y'] * DEG_TO_RAD * (dt_ms / 1000))) + p2 * atan_rad
+			angle_deg = angle_rad * RAD_TO_DEG
 
 			# PID
-			error = target - angle
-			integral = integral + error * dt
-			derivative = (error - previous_error)/dt
+			error = target_deg - angle_deg
+			integral = integral + error * dt_ms
+			derivative = gyro['y']
+			#derivative = (error - previous_error)/dt_ms
 			#speed_percent = Kp * error + Ki * integral + Kd * derivative
 			diff_speed = Kp * error + Ki * integral + Kd * derivative
 			previous_error = error
@@ -150,8 +164,8 @@ try:
 			stdscr.addstr(12, 9, "[%.4f%% (%d)]" % (speed_percent, pos))
 
 			stdscr.addstr(14, 8, "PID:")
-			stdscr.addstr(15, 9, "[DT:%.3f]" % (dt))
-			stdscr.addstr(16, 9, "[Erreur:%.3f] [Avgy:%.3f] [Atan:%.3f] [Asin:%.3f] [P1:%.3f] [P2:%.3f]" % (error, angle_rad, atan, asin, p1, p2))
+			stdscr.addstr(15, 9, "[DT:%.3f]" % (dt_ms))
+			stdscr.addstr(16, 9, "[Erreur:%.3f] [Avgy:%.3f] [Atan:%.3f] [Asin:%.3f] [P1:%.3f] [P2:%.3f]" % (error, angle_rad, atan_rad, asin_rad, p1, p2))
 			stdscr.addstr(17, 9, "[Integral:%.3f Derivative:%.3f]" % (integral, derivative))
 			stdscr.addstr(18, 9, "[DIFFSPEED:%.3f]" % (diff_speed))
 			stdscr.addstr(19, 9, "[Kp*E:%.6f]" % (Kp * error))
@@ -166,7 +180,7 @@ try:
 		line = []
 		ts = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S_%f')
 		line.append(ts)
-		reltime = time.time()*100000 - start_time
+		reltime = time.time()*1000000 - start_time
 		line.append(reltime)
 		line.append(pos)
 		line.append(axis['x'])
@@ -179,9 +193,9 @@ try:
 		line.append(mpu_accel['y'])
 		line.append(mpu_accel['z'])
 		line.append(round(distance,2))
-		line.append(round(atan*28.66,2))
-		line.append(round(asin*28.66,2))
-		line.append(angle)
+		line.append(round(atan_rad * RAD_TO_DEG,2))
+		line.append(round(asin_rad * RAD_TO_DEG,2))
+		line.append(angle_deg)
 		line.append(angle_rad)
 		line.append(error)
 		line.append(integral)
@@ -189,11 +203,14 @@ try:
 		line.append(Kp)
 		line.append(Ki)
 		line.append(Kd)
-		line.append(dt)
+		line.append(dt_ms)
 		line.append(diff_speed)
 		lines.append(line)
 
-		time.sleep(0.05)
+		sleep_time = 0.05 - (time.time() - init_time)
+		if sleep_time < 0
+			sleep_time = 0
+		time.sleep(sleep_time)
 except Exception as e:
 	if enable_curse:
 		curses.endwin()
